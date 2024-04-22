785945b28b085e31cb57546a7301412440607e9a
Soleil <solangegarciaguamanga@gmail.com>
2024-04-22 01:12:02 -0500

    add router --> index mopi

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const e = require("express")
+const express = require("express")
+const app = express()
+const router = require('../Mopi/src/routers/indexMopi')
+const PORT = 3003
+
+app.use(express.json())
+app.use('/app', express.static("../Mopi/screens/pantallaInicial"))
+
+app.use('/api', router)
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}/app`)
+})
