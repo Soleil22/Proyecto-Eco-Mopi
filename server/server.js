b5823c62a45862cf087cdc73f9bb81a604b170cc
Soleil <solangegarciaguamanga@gmail.com>
2024-04-22 01:14:16 -0500

    its working

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const router = require('../Mopi/src/routers/indexMopi')
 const PORT = 3003
 
 app.use(express.json())
-app.use('/app', express.static("../Mopi/screens/pantallaInicial"))
+app.use('/app', express.static("./Mopi/screens/pantallaInicial"))
 
 app.use('/api', router)
 
